b712e9ac4e0357d61e4b70b7593fb2ab03e13da6
Jalaj Pratap Singh <jalajpratapsingh.alg@gmmail.com>
2020-06-04 22:41:44 +0530

    Backend With Express and MongoDb

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+//get post
+router.get("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+//addpost
+
+router.post("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+  res.status(201).send();
+});
+
+//delete post
+
+router.delete("/id:", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://roger:qweasdzxc@mongovue-jvjfq.mongodb.net/mongo_vue?retryWrites=true&w=majority",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  );
+  return client.db("mongo_vue").collection("posts");
+}
+
+module.exports = router;
