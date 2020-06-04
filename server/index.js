3f0035f343fac93e83370c599ea885bdbacd941c
Jalaj Pratap Singh <jalajpratapsingh.alg@gmmail.com>
2020-06-05 02:05:10 +0530

    Production mode

--- server/index.js
+++ server/index.js
@@ -12,6 +12,14 @@ app.use(cors());
 const posts = require("./routes/api/posts");
 app.use("/api/posts", posts);
 
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  //handle Single page applicaiton
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
